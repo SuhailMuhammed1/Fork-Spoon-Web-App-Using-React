00ef79e1cd71c6a2dd2fb4410206282505932213
SuhailMuhammed1 <suhailjr111@gmail.com>
2024-10-16 13:08:09 +0530

    created welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header/Header'
+import Welcome from './pages/Welcome/Welcome'
 
 function App() {
   return (
     <>
     <Header/>
+    <Welcome/>
     </>
   )
 }
